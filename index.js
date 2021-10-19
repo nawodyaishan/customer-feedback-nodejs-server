12751bc1427e1de2df2db9587663d68c47f6a6e8
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2021-10-19 15:58:15 +0530

    Update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').strategy;
+
 const app = express();
 
 passport.use(new GoogleStrategy(
