852ef52e0d00f54e2c7abb439361707cb44c5b96
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2021-10-19 16:11:50 +0530

    Update

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ const keys = require('./config/keys');
 const app = express();
 
 passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'},
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    },
     accessToken => {
-    console.log(accessToken);
+        console.log(accessToken);
     }
 ));
+
+app.get('/auth/google', passport.authenticate('google',{
+    scope: ['profile', 'email']
+}));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT)
