9dac9928d85266b1829c91e50badc6c9e69a461b
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2021-10-19 15:54:07 +0530

    Update

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID: '652442102279-kn3s5o20310j5la4osurq5pj1gu07rs9.apps.googleusercontent.com',
+    googleClientSecret: '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'
+};
+
+
+
